e0cf870927d2fa448954fe6553723f7cc6e87f42
unknown <tianhew0121>
2021-12-01 03:04:04 -0800

    Format

--- CS161 Algorithm/Dynamic Programming/Dynamic.py
+++ CS161 Algorithm/Dynamic Programming/Dynamic.py
@@ -1,14 +1,15 @@
 # This file has an implementation of dynamic programing version of Truck Loading (subset-sum) problem.
-#Problem definition:
+# Problem definition:
 # Truck has weight limit of W .
 #   n boxes. Box i has weight wi.
 #   We want to load the truck to carry the maximum weight
 #   possible, subject to the weight restriction.
 
 W = 100
-w = [0, 90,90]
-OPT = None # 2d matrix, n x W    n number of box, W weight limit of truck
+w = [0, 90, 90]
+OPT = None  # 2d matrix, n x W    n number of box, W weight limit of truck
 n = len(w)-1
+
 
 def matrix(row, col, val):
     lst = list()
@@ -20,8 +21,8 @@ def matrix(row, col, val):
 
 
 def dynamic_load_truck(W, w, n):
-    OPT = matrix(n,W,0)
-    keep = matrix(n,W,0)
+    OPT = matrix(n, W, 0)
+    keep = matrix(n, W, 0)
     for i in range(1, n+1):
         for j in range(1, W+1):
             if w[i] > j or (w[i] + OPT[i-1][j-w[i]] <= OPT[i-1][j]):
@@ -32,14 +33,17 @@ def dynamic_load_truck(W, w, n):
                 keep[i][j] = True
     return OPT, keep
 
+
 def output_result(OPT, keep, i, j):
     #i, j = i+1, j+1
-    if i == 0: return
+    if i == 0:
+        return
     if keep[i][j]:
-        output_result(OPT,keep,i-1,j-w[i])
+        output_result(OPT, keep, i-1, j-w[i])
         print(w[i])
     else:
-        output_result(OPT, keep,i-1,j)
+        output_result(OPT, keep, i-1, j)
+
 
 OPT, keep = dynamic_load_truck(W, w, n)
-output_result(OPT,keep,n,W)
+output_result(OPT, keep, n, W)
--- CS161 Algorithm/Dynamic Programming/MatrixChainMult.py
+++ CS161 Algorithm/Dynamic Programming/MatrixChainMult.py
@@ -1,6 +1,7 @@
 from prettytable import PrettyTable
 #d = (40,2,100,50)
-d = (10,15,5,60,100,20,40,47)
+d = (10, 15, 5, 60, 100, 20, 40, 47)
+
 
 def matrix(row, col, val):
     lst = list()
@@ -10,39 +11,40 @@ def matrix(row, col, val):
             lst[r].append(val)
     return lst
 
+
 def optMatrixChain(d):
     n = len(d) - 1
-    M = matrix(n,n,0)
-    S = matrix(n,n,0)
-    for length in range(2,n+1):
+    M = matrix(n, n, 0)
+    S = matrix(n, n, 0)
+    for length in range(2, n+1):
         for i in range(1, n-length+2):
             j = i+length-1
             M[i][j] = float('inf')
-            for k in range(i,j):
+            for k in range(i, j):
                 x = M[i][k] + M[k+1][j] + d[i-1] * d[k] * d[j]
                 print('x:', x)
                 print('k:', k)
                 print('i, j:', i, j)
-                
-                
+
                 if x < M[i][j]:
                     M[i][j] = x
                     S[i][j] = k
                 x = PrettyTable()
                 for row in M[1:]:
                     x.add_row(row[1:])
-                    #x.add_row(row)
-                print(x,'\n')
+                    # x.add_row(row)
+                print(x, '\n')
     return M[1:], S[1:]
+
 
 M, S = optMatrixChain(d)
 x = PrettyTable()
 for row in M:
-    #x.add_row(row[1:])
+    # x.add_row(row[1:])
     x.add_row(row)
 print(x)
 x = PrettyTable()
 for row in S:
-    #x.add_row(row[1:])
+    # x.add_row(row[1:])
     x.add_row(row)
 print(x)
